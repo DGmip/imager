4e41460e4372d7422526b29dd3e2525765f8d7ff
ben <bwall256@gmail.com>
2015-09-03 02:01:33 +0200

    curl writes to a file

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+  "io/ioutil"
+  // "time"
+  // "strings"
   "bytes"
-  "fmt"
+  // "fmt"
   "os/exec"
   // for os.Args
   "os"
@@ -11,11 +14,19 @@ import (
 
 func  main() {
 
+  // taking args from command line
   arg := os.Args
-  cmd := exec.Command("curl "+arg[1])
+  cmd := exec.Command("curl", arg[1])
   var out bytes.Buffer
   cmd.Stdout = &out
   err := cmd.Run();if err != nil { log.Fatal(err) }
-  fmt.Println(out.String())
-
+  // lines := strings.Split(out.String(), "\n")
+  err = ioutil.WriteFile(arg[2], out.Bytes(), 0644)
+  // for _, line := range lines {
+  //   fmt.Println(line)
+    // if strings.Contains(line, "css"){
+    //   fmt.Println(line)
+    //   time.Sleep(1 * time.Second )
+    // }
+  // }
 }
